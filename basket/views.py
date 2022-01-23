44281ae00f5ee3617b3002db4edc32e4fad60def
lokendra <mahatlokendra2051@gmail.com>
2022-01-23 14:18:52 +0545

    added basket functionalityusing jquery, ajax and django session

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse, JsonResponse
+from .basket  import Basket 
+from store.models import Product
 
 # Create your views here.
 
 
 def basket_summary(request):
     return render(request,'store/basket/basket_summary.html',{})
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product = product, qty = product_qty)
+        response = JsonResponse({'qty':product_qty})
+        return response
